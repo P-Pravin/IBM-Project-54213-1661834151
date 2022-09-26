ca495eef93d0c48269ab51df99b098888589ad9e
Vigneshsr07 <113779514+Vigneshsr07@users.noreply.github.com>
2022-09-26 20:55:07 +0530

    Add files via upload

---
+++ Assesments/Team Leader-Vignesh SR/Assignment 2/Output Asignment 2.py
@@ -0,0 +1,11 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:59:51) [MSC v.1914 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:\Users\Nalla pulla\AppData\Local\Programs\Python\Python37\python tempp.py 
+high temperature and humidity of: 94 60 % alarm is on
+high temperature and humidity of: 73 56 % alarm is on
+high temperature and humidity of: 81 70 % alarm is on
+high temperature and humidity of: 46 78 % alarm is on
+high temperature and humidity of: 36 71 % alarm is on
+Low temperature and humidity of: 28 24 % alarm is off
+>>> 
