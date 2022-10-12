b10dbd50ba0fec8ed1e409e9563f81baf00ef13c
Vickyvignesh6111 <113778128+Vickyvignesh6111@users.noreply.github.com>
2022-10-12 16:47:29 +0530

    Add files via upload

---
+++ Assesments/M2-Lead-Vignesh M/Assignment 3/Blinking LED.py
@@ -0,0 +1,24 @@
+mport serial  
+ import time    
+   
+ arduino = serial.Serial('com4',9600) 
+ time.sleep(2) 
+  
+ print arduino.readline()  
+ print ("Enter 1 to get LED ON & 0 to get OFF") 
+  
+   
+ while 1:       
+  
+     var = raw_input()  
+     
+      
+     if (var == '1'):  
+         arduino.write('1')  
+         print ("LED turned ON") 
+         time.sleep(1) 
+      
+     if (var == '0'):  
+         arduino.write('0')  
+         print ("LED turned OFF") 
+         time.sleep(1)
